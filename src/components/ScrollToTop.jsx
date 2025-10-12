c43eb62abb02c9989cdc79e1559232f8af6f5bc2
GriffonSystems <paul@griffonsys.com>
2025-10-12 17:58:12 -0500

    Update ScrollToTop.jsx

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,1 +1,11 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
+export default function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    // jump to top on each route change
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+  }, [pathname])
+  return null
+}
