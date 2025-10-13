551f3047023c21f7160d11d92676379da7818ea4
GriffonSystems <paul@griffonsys.com>
2025-10-13 15:47:34 -0500

    Update strings.js

--- src/strings.js
+++ src/strings.js
@@ -1,1 +1,3 @@
-
+// src/config/strings.js
+export const HERO_TITLE = 'Security Experts for 20+ Years'
+export const HERO_SUBTITLE = 'Video Surveillance, Access Control, Intercom -- Avigilon, Verkada'
