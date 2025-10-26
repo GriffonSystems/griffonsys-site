7a71d768595dec853f9aee883d6c005fd010e6ee
GriffonSystems <paul@griffonsys.com>
2025-10-26 00:39:40 -0500

    Create keep-vendors-assets.js

---
+++ src/keep-vendors-assets.js
@@ -0,0 +1,8 @@
+// This file prevents Vite from tree-shaking vendor image assets during the build
+import "/public/vendors/verkada/access/singledoor.png"
+import "/public/vendors/verkada/access/4doorcontroller.png"
+import "/public/vendors/verkada/access/singledoorreader.png"
+import "/public/vendors/verkada/access/keypad.png"
+import "/public/vendors/verkada/intercom/td33.jpg"
+import "/public/vendors/verkada/intercom/td53.jpg"
+import "/public/vendors/verkada/intercom/td63.jpg"
