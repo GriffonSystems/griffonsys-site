01533e583e4d84332eba993ca1d44c2a82c5a160
GriffonSystems <paul@griffonsys.com>
2026-01-18 09:52:21 -0600

    Update Canonical.jsx

--- src/components/Canonical.jsx
+++ src/components/Canonical.jsx
@@ -1,13 +1,10 @@
-import { Helmet } from "react-helmet-async"
+import { Helmet } from "react-helmet"
 import { useLocation } from "react-router-dom"
 
 export default function Canonical() {
   const { pathname } = useLocation()
-
-  // No trailing slash except root
   const cleanPath =
     pathname !== "/" && pathname.endsWith("/") ? pathname.slice(0, -1) : pathname
-
   const canonical = `https://www.griffonsys.com${cleanPath}`
 
   return (
