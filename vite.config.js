72c52b3d39bb9d16d30e48e193a86dcd6ec7db20
GriffonSystems <paul@griffonsys.com>
2026-01-25 12:20:07 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,53 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import rollupNodePolyfills from "rollup-plugin-polyfill-node"
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill"
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
+      // handle both "stream" and "node:stream" style imports
       stream: "stream-browserify",
-      buffer: "buffer"
+      "node:stream": "stream-browserify",
+
+      buffer: "buffer",
+      "node:buffer": "buffer",
+
+      process: "process/browser",
+      "node:process": "process/browser",
+
+      util: "util",
+      "node:util": "util",
+
+      events: "events"
     }
   },
+
   define: {
-    global: "window"
+    global: "globalThis"
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis"
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyfills()]
+    }
   }
 })
